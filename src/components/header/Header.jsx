c2bd732398cf3e22093682ab09f0d15e9efefc69
thealihamza04 <ah0681988@gmail.com>
2025-11-02 23:48:53 +0500

    update ui

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,6 +5,7 @@ import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { AnimateIcon } from "@/components/ui/animate-ui/icons/icon.tsx"
 import { MessageSquareText } from "@/components/ui/animate-ui/icons/message-square-text.tsx"
+import { ClipboardList } from "@/components/animate-ui/icons/clipboard-list.tsx"
 import { List } from "@/components/ui/animate-ui/icons/list.tsx"
 
 
@@ -17,7 +18,7 @@ const Header = () => {
             <div className="fixed top-4 right-4 md:right-6 z-10">
                 <Link to={location.pathname === "/" ? "/chat" : "/"}>
                     <Button variant="outline" size="xl" className="p-3 cursor-pointer">
-                        {location.pathname === "/" ? <MessageSquareText /> : <List />}
+                        {location.pathname === "/" ? <MessageSquareText /> : <List animation="path-loop" />}
                     </Button>
                 </Link>
             </div>
