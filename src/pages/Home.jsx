fd5844e2079ecabd9b42e0a9d4192fdc999881fc
The Ali Hamza <ah0681988@gmail.com>
2025-12-13 07:58:31 +0500

    Add first-visit instructions dialog

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { cn } from "@/utils/cn.js";
 import Heading from "@/components/ui/Heading";
 import CreateTodo from "../components/todo/CreateTodo";
 import AllTodos from "../components/todo/AllTodos";
 import { Firebase } from "@/components/ui/icons/firebase.jsx"
 import { ReactIcon } from "@/components/ui/icons/react.jsx"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
 
 
 const Home = () => {
+    const INSTRUCTIONS_KEY = "todo-app-instructions-seen";
+    const [showInstructions, setShowInstructions] = useState(false);
+
+    useEffect(() => {
+        const hasSeenInstructions = localStorage.getItem(INSTRUCTIONS_KEY);
+
+        if (!hasSeenInstructions) {
+            setShowInstructions(true);
+        }
+    }, []);
+
+    const dismissInstructions = () => {
+        localStorage.setItem(INSTRUCTIONS_KEY, "true");
+        setShowInstructions(false);
+    };
+
     return (
         <div
             className={cn(
@@ -18,6 +43,35 @@ const Home = () => {
                 "lg:px-24 lg:py-18" // large screens
             )}
         >
+
+            <Dialog open={showInstructions} onOpenChange={dismissInstructions}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Welcome to the Firebase Todo App</DialogTitle>
+                        <DialogDescription>
+                            Here&apos;s a quick guide to help you get started. We&apos;ll only
+                            show this once.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <ul className="space-y-3 text-sm text-muted-foreground">
+                        <li>
+                            <strong>Add tasks:</strong> Use the input at the bottom to
+                            write a todo and press Enter or click Add.
+                        </li>
+                        <li>
+                            <strong>Organize your list:</strong> Check items to mark
+                            them complete and use the filters to view what matters.
+                        </li>
+                        <li>
+                            <strong>Stay synced:</strong> All your changes are saved in
+                            Firebase so you can pick up where you left off.
+                        </li>
+                    </ul>
+                    <DialogFooter>
+                        <Button onClick={dismissInstructions}>Got it</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
 
             {/* <Heading
                 size='sm'
